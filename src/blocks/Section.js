188e52a79520e3c548eace023963cd84c4d76c70
Holger H <holgheiss@gmail.com>
2020-04-19 19:52:58 +0200

    avoid Header trouble

--- src/blocks/Section.js
+++ src/blocks/Section.js
@@ -2,7 +2,7 @@ import React from "react"
 import styled from 'styled-components'
 
 import {p as P} from '../components/HtmlElements'
-import { Header } from '../components'
+import Header from '../components/Header'
 
 const blockLabel = "SECTION"
 
