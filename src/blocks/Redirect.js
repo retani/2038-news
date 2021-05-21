1899f54b8037696e5c29bcc8115553b5d0c11e4c
Holger H <holgheiss@gmail.com>
2021-05-21 08:30:01 +0200

    Redirect to subpage REDIRECT, slicker style

--- src/blocks/Redirect.js
+++ src/blocks/Redirect.js
@@ -4,34 +4,75 @@ import styled from 'styled-components'
 import { BlockListItem, BlockWrapper } from '../components'
 
 import { genericFields } from '../helpers/misc'
+import { colors, spaces, typoSizes, blockSnippet, blockTypoSnippet, typoStyles, breakpoints, typoSnippet } from '../../config/styles'
 
-const blockLabel = "Redirect"
+const blockLabel = "Subpage REDIRECT"
 
 export function Redirect({ data }) {
-  const { text, hide } = data
+  const { text, text2, hide } = data
   const url = text
   const edit = /*( show || cms.sidebar.isOpen ) && */ process.env.NODE_ENV === "development"
 
-  const info = <BlockWrapper label={blockLabel} hide={hide}>
-    <A href={url}>
-      This page will redirect to {url}
-    </A>
-  </BlockWrapper>
+  const info = 
+    <Asmall href={url}>
+      {url}
+    </Asmall>
+  
 
-  const action = <>
-    <a href={url}>
-      Please click here if you are not redirected to {url}
-    </a>
+  const action =
     <Helmet>
-      {url && <meta http-equiv="refresh" content={`0; url = ${url}`} />}
+      {url && <meta http-equiv="refresh" content={`2; url = ${url}`} />}
     </Helmet>
-  </>
 
-  return edit ? info : hide ? null : action
+  const content = 
+    <Container>
+      <A href={url}>
+        {text2}
+      </A>
+      { edit || hide || action }
+    </Container>
+  
+  
+  return <BlockWrapper label={blockLabel} hide={hide}>
+    { content }
+  </BlockWrapper>
+  
 };
 
 const A = styled.a`
+  white-space: pre-line;
+  ${
+    blockSnippet({
+      spaceSide: spaces.large
+    })
+  };
+  color: ${ colors.turquoise };
+`
+
+const Asmall = styled.a`
   word-wrap: break-word;
+  ${blockSnippet({
+    spaceSide: spaces.small,
+    spaceSide: spaces.small
+  })
+  };
+  color: ${colors.bg};
+  font-size: 0.5rem;
+  position: absolute;
+  top: 0;
+  opacity: 0.4;
+`
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  top:0;
+  left: 0;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  place-content: center;
+
 `
 
 export const RedirectBlock = {
@@ -42,9 +83,11 @@ export const RedirectBlock = {
   }),
   defaultItem: {
     text: "https://2038.xyz",
+    text2: "Redirecting...",
   },
   fields: [
     { name: "text", label: "URL", component: "text" },
+    { name: "text2", label: "Text", component: "textarea" },
     ...genericFields
   ],
 }
